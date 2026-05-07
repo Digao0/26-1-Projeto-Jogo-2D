cc7b2854174d12903c27f471ce7e79ac9c4c757f
Gutasss <gutmota23@gmail.com>
2026-05-07 09:40:23 -0300

    enemy died

--- Assets/Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -58,7 +58,7 @@ public class EnemyHealth : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        FindObjectOfType<WaveManager>().EnemyDied();
+        FindObjectOfType<WaveManager>()?.EnemyDied();
         Destroy(gameObject);
     }
 }
